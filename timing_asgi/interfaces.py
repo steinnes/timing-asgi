f69b40ce88f5f1c876a3c7433a72e83e0a6f0f4c
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2019-03-06 17:43:17 +0000

    Get back to 100% coverage

--- timing_asgi/interfaces.py
+++ timing_asgi/interfaces.py
@@ -6,11 +6,11 @@ from typing import List
 class MetricNamer(ABC):
     @abstractmethod
     def __call__(self, scope) -> str:
-        pass
+        pass  # pragma: no cover
 
 
 class TimingClient(ABC):
     """ An abstract class detailing the client interface """
     @abstractmethod
     def timing(self, metric_name: str, timing: float, tags: List[str]):
-        pass
+        pass  # pragma: no cover
