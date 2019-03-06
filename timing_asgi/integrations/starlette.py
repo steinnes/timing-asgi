62fd226c0c89aa58588ba85d046f1ef10185eadd
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2019-03-06 18:10:11 +0000

    Update StarletteScopeToName implementation to use the route name

--- timing_asgi/integrations/starlette.py
+++ timing_asgi/integrations/starlette.py
@@ -5,6 +5,14 @@ from timing_asgi.utils import PathToName
 
 
 class StarletteScopeToName(MetricNamer):
+    """ Class which extracts a suitable metric name from a matching Starlette route.
+
+    Uses the route name if it has one, but otherwise constructs a name based on the
+    full module + method/class path.
+
+    Falls back to either a provided fallback callable, or uses PathToName, which is the
+    TimingMiddleware default.
+    """
     def __init__(self, prefix, starlette_app, fallback=None):
         self.prefix = prefix
         self.starlette_app = starlette_app
@@ -19,6 +27,6 @@ class StarletteScopeToName(MetricNamer):
                 route = r
                 break
         if route is not None:
-            return f"{self.prefix}.{route.endpoint.__module__}.{route.endpoint.__name__}"
+            return f"{self.prefix}.{route.endpoint.__module__}.{route.name}"
         else:
             return self.fallback(scope)
